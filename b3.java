6864b5abe4547de1ee8b444bd06f4fa921bb8f42
thuhuyen03 <125672051+thuhuyen03@users.noreply.github.com>
2023-03-07 15:05:48 +0700

    Add files via upload

---
+++ b3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class b3 {
+	 public static void main(String[] args) {
+	        String name;
+	        int year;
+	        Scanner input = new Scanner (System.in);
+	        System.out.println("Nhap Ten:");
+	        name = input.nextLine();
+	        System.out.println("Nhap Nam Sinh:");
+	        year = input.nextInt();
+	        if(2023 - year  < 16) System.out.println("Ban " + name + " o do tuoi vi thanh nien");
+	        else if(2023 - year < 18) System.out.println("Ban " + name + " o do tuoi truong thanh");
+	        else System.out.println("Ban " + name + " da gia");
+	    }
+
+}
---
+++ b4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class b4 {
+	 public static void main(String[] args) {
+	        int n;
+	        Scanner input = new Scanner(System.in);
+	        do {
+	            System.out.println("Nhap n:");
+	            n = input.nextInt();
+	        } while(n < 1 || n > 12);
+	        System.out.println("Thang " + n);
+	    }
+
+}
